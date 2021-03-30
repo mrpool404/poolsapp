e54d3bb122d73658eb01b657a7c99603e717456f
mrpool404 <arghodaketech@gmail.com>
2021-03-30 22:16:12 +0530

    Disabling getConfig

--- Pools-App-Backend/ManagementRoutes.js
+++ Pools-App-Backend/ManagementRoutes.js
@@ -2,13 +2,13 @@ const routes = require("express").Router();
 const fs = require('fs')
 
 // Returns the config
-routes.get('/getConfig', async(req, res) => {
-    res.status(200).send({
-        "status": 200,
-        "authEnabled": req.app.get('authEnabled'),
-        "dbEnabled": req.app.get('dbEnabled')
-    })
-})
+// routes.get('/getConfig', async(req, res) => {
+//     res.status(200).send({
+//         "status": 200,
+//         "authEnabled": req.app.get('authEnabled'),
+//         "dbEnabled": req.app.get('dbEnabled')
+//     })
+// })
 
 // Updates the config and writes it onto the file
 routes.post('/updateConfig', async(req, res) => {
